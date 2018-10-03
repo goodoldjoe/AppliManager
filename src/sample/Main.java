c1018a02dd105cf0566e12662e55700fe91d4e72
goodoldjoe <studer.ronald@gmail.com>
2018-10-03 19:59:44 +0200

    query funktioniert

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,10 +10,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent login = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(login, 300, 275));
         primaryStage.show();
+
+
     }
 
 
