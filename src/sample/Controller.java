62e865b67abec01c6829e0dd69d932ccb9bf1ef8
mesopotato <dittrich.yannick@gmail.com>
2018-10-24 15:31:30 +0200

    logic

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,51 @@
 package sample;
 
+import javafx.fxml.FXML;
+import java.io.IOException;
+import java.util.Optional;
+
+import com.sun.javafx.logging.Logger;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.ChoiceDialog;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
 public class Controller {
-	Int kakk;
-	int salat;
+    @FXML
+    private javafx.scene.control.Label status, label;
+    @FXML private javafx.scene.control.TextField textfield, text;
+    @FXML private javafx.scene.control.Button btn1, btn2;
+
+    public Main main;
+
+    //das erlaubt uns auf alle methoden aus der Main kalsse zugreifen zu können
+    public void setMain(Main main) {
+        this.main = main;
+
+        btn1.setOnAction(event -> {
+            main.newWindow();
+        });
+
+        btn2.setOnAction(event -> {
+            System.out.println("ey");
+        });
+    }
+
+    public void setNewWindow(Main main) {
+        this.main = main;
+
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,22 +1,57 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import java.io.IOException;
 
-public class Main extends Application {
+public class    Main extends Application {
+    Stage window;
+    Stage window2 = new Stage();
+    @FXML private javafx.scene.control.Button  button;
+
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello Sir");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        window = primaryStage;
+        mainWindow();
     }
+    public void mainWindow(){
+        try{
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("sample.fxml"));
+        AnchorPane pane = loader.load();
+        window.setTitle("Hello Sir");
 
+        Controller controller = loader.getController();
+        controller.setMain(this);
 
+        window.setScene(new Scene(pane, 600, 400));
+        window.show();
+
+    }catch(IOException e) {
+            e.printStackTrace();
+        }
+        }
+    public void newWindow(){
+        FXMLLoader root2 = new FXMLLoader(Main.class.getResource("sample2.fxml"));
+        try {
+            AnchorPane pane = root2.load();
+            window.setTitle("Hello Sir");
+
+            Controller controller = root2.getController();
+            controller.setNewWindow(this);
+            window.setScene(new Scene(pane, 600, 400));
+            //nur für die zweiten fenster
+            //window2.show();
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
         launch(args);
     }
